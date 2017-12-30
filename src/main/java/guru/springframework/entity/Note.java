90cc8295d08ace498da69a6ba1f12e9841f27b8b
Edward <e.slavov11@gmail.com>
2017-12-30 18:42:08 +0200

    ingredients added

--- src/main/java/guru/springframework/entity/Note.java
+++ src/main/java/guru/springframework/entity/Note.java
@@ -8,19 +8,12 @@ public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String description;
-    private Integer prepTime;
-    private Integer cookTime;
-    private Integer servings;
-    private String source;
-    private String url;
-    private String directions;
+
+    @OneToOne
+    private Recipe recipe;
 
     @Lob
-    private Byte[] image;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private Note note;
+    private String recipeNotes;
 
     public Long getId() {
         return id;
@@ -30,75 +23,19 @@ public class Note {
         this.id = id;
     }
 
-    public String getDescription() {
-        return description;
+    public Recipe getRecipe() {
+        return recipe;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
     }
 
-    public Integer getPrepTime() {
-        return prepTime;
+    public String getRecipeNotes() {
+        return recipeNotes;
     }
 
-    public void setPrepTime(Integer prepTime) {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime() {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime) {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServings() {
-        return servings;
-    }
-
-    public void setServings(Integer servings) {
-        this.servings = servings;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getDirections() {
-        return directions;
-    }
-
-    public void setDirections(String directions) {
-        this.directions = directions;
-    }
-
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Note getNote() {
-        return note;
-    }
-
-    public void setNote(Note note) {
-        this.note = note;
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
     }
 }
--- src/main/java/guru/springframework/entity/Recipe.java
+++ src/main/java/guru/springframework/entity/Recipe.java
@@ -1,18 +1,30 @@
 package guru.springframework.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "recipes")
 public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
 
-    @OneToOne
-    private Recipe recipe;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
-    private String recipeNotes;
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Note note;
 
     public Long getId() {
         return id;
@@ -22,19 +34,83 @@ public class Recipe {
         this.id = id;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
+    public String getDescription() {
+        return description;
     }
 
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getRecipeNotes() {
-        return recipeNotes;
+    public Integer getPrepTime() {
+        return prepTime;
     }
 
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
